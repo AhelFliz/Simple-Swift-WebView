fef2a62273bb5b399207cf3b376635237bc77ac8
AhelFliz <ahelfliz@gmail.com>
2024-10-13 10:57:35 +0200

    First commit

---
+++ oai appTests/oai_appTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import oai_app
+
+struct oai_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
